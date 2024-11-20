2de48d6c1f6ab87d95e675c2c6010eadf0496fe5
Ninfa-C <ninfa_95@hotmail.it>
2024-11-20 10:48:24 +0100

    lastPushGiuro

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,20 @@
-const welcomeCheck = document.getElementById("welcomeCheck");
-const btnWelcomeProceed = document.getElementById("btnWelcomeProceed");
-const errorCheck = document.getElementById("errorCheck");
+const welcomeCheck = document.getElementById('welcomeCheck');
+const btnWelcomeProceed = document.getElementById('btnWelcomeProceed');
+const errorCheck = document.getElementById('errorCheck');
 
-document.addEventListener("load", init());
+document.addEventListener('load', init());
 function init() {
   btnWelcomeProceed.disabled = true;
-  errorCheck.style.display = "none";
+  errorCheck.style.display = 'none';
 }
 
 function boxCheck() {
-  if (welcomeCheck.checked) btnWelcomeProceed.disabled = false;
-  else {
+  if (welcomeCheck.checked) {
+    btnWelcomeProceed.disabled = false;
+    errorCheck.style.display = 'none';
+  } else {
     btnWelcomeProceed.disabled = true;
-    errorCheck.style.display = "block";
+    errorCheck.style.display = 'block';
   }
 }
 
